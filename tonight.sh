55134c6b5ef60d0d297220322899d9463e97aead
NotHax <53000121+NotHax@users.noreply.github.com>
2019-08-07 20:39:30 +0700

    Update tonight.sh

--- tonight.sh
+++ tonight.sh
@@ -8,21 +8,54 @@ y="\033[1;93m"
 b="\033[1;34m"
 r="\033[1;31m"
 c="\033[1;36m"
+g="\033[32m"
 
+clear
 echo ""
-echo $c "**********                    **         **                    **              **  **               
-echo $y "/////$c**$y///                    /$c**        $y/$c**                  $y/$c**             $y/$c** $y/$c**               
-echo $y "    /$c**      ******   ******  $y/$c**  ******$y/$c** *******   ****** ******  ******   $y/$c** $y/$c**  *****  ******
-echo $y "    /$c**     **$y////$c** **$y////$c** $y/$c** **$y//// /$c**$y//$c**$y///$c** **/$y/// ///$c**$y/  //////$c**  $y/$c** $y/$c** **$y///$c**$y//$c**$y//$c*
-echo $y "    /$c**    $y/$c**   $y/$c**$y/$c**   $y/$c** $y/$c**$y//$c***** $y/$c** $y/$c**  $y/$c**$y//$c*****   $y/$c**    *******  $y/$c** $y/$c**$y/$c******* $y/$c** $y/ 
-echo $y "    /$c**    $y/$c**   $y/$c**$y/$c**   $y/$c** $y/$c** $y/////$c**$y/$c** $y/$c**  $y/$c** $y/////$c**  $y/$c**   **$y////$c**  $y/$c** $y/$c**$y/$c**$y////  /$c**   
-echo $y "    /$c**    $y//$c****** $y//$c******  *** ****** $y/$c** ***  $y/$c** ******   $y//$c** $y//$c******** *** ***$y//$c******$y/$c***   
-echo $y "    //      //////   //////  /// //////  // ///   // //////     //   //////// /// ///  ////// ///    
+echo $c "**********                    **         **                    **              **  **               "
+echo $y "/////$c**$y///                    /$c**        $y/$c**                  $y/$c**             $y/$c** $y/$c**               "
+echo $y "    /$c**      ******   ******  $y/$c**  ******$y/$c** *******   ****** ******  ******   $y/$c** $y/$c**  *****  ******"
+echo $y "    /$c**     **$y////$c** **$y////$c** $y/$c** **$y//// /$c**$y//$c**$y///$c** **/$y/// ///$c**$y/  //////$c**  $y/$c** $y/$c** **$y///$c**$y//$c**$y//$c*"
+echo $y "    /$c**    $y/$c**   $y/$c**$y/$c**   $y/$c** $y/$c**$y//$c***** $y/$c** $y/$c**  $y/$c**$y//$c*****   $y/$c**    *******  $y/$c** $y/$c**$y/$c******* $y/$c** $y/ "
+echo $y "    /$c**    $y/$c**   $y/$c**$y/$c**   $y/$c** $y/$c** $y/////$c**$y/$c** $y/$c**  $y/$c** $y/////$c**  $y/$c**   **$y////$c**  $y/$c** $y/$c**$y/$c**$y////  /$c**   "
+echo $y "    /$c**    $y//$c****** $y//$c******  *** ****** $y/$c** ***  $y/$c** ******   $y//$c** $y//$c******** *** ***$y//$c******$y/$c***   "
+echo $y "    //      //////   //////  /// //////  // ///   // //////     //   //////// /// ///  ////// ///    "
 echo ""
 sleep 1
-echo " ╔═══════════════════════════════════════════╗
-echo "  【Author】: NotGuardian
-echo "  【Github】: https://github.com/NotHax
-echo "  【Instagram】 : https://instagram.com/notfuardianyt
-echo "  【Youtube】: Not Guardian Hax
-echo " ╚═══════════════════════════════════════════╝
+echo " $c╔═══════════════════════════════════════════╗"
+echo "  $g【$rAuthor$g】$c: $bNotGuardian"
+echo "  $g【$rGithub$g】$c: $ghttps://github.com/NotHax"
+echo "  $g【$rInstagram$g】$c: $ghttps://instagram.com/notfuardianyt"
+echo "  $g【$rYoutube$g】$c: $rNot Guardian Hax"
+echo " $c╚═══════════════════════════════════════════╝"
+sleep 1
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo -e $r"[!]> (Ctrl + C ) Detected, Trying To Exit ... "
+exit
+}
+echo
+echo "$c╔═══════════════════╗"
+echo "$r     Tools Installer"
+echo "$c╚═══════════════════╝"
+sleep 1
+echo "$c [$b01$c] $gNmap"
+
+echo "$c [$b00$c] $rExit"
+echo ""
+echo  "╭─Pilih─|lolcat
+read -p "╰─>" pil; 
+
+#Nmap
+case $pil in
+01) echo "Gak Ada Goblok!1!1!"
+
+;;
+
+#Exit
+00) echo " Bye Pantek!!!"
+exit
